b48cd411fb18922e71cd5cd151ed4b9d932ec197
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-04-05 14:53:13 -0300

    Update script.js

--- template-string/script.js
+++ template-string/script.js
@@ -1,3 +1,13 @@
 let nome = 'Jorge'
 
-console.log( 'Oi ' + nome + ', tudo bem?')
+//console.log( 'Oi ' + nome + ', tudo bem?')
+console.log( `Oi ${nome}, tudo bem?`)
+
+
+console.log(` 23 + 45 = ${23 + 45}`)
+
+function calcular( x, y){
+    return x * y
+}
+
+console.log(`A multiplicação de 9 + 9 é: ${calcular(8,8)} `)
