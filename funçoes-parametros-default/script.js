2e4971d72eb9010f59b8ee2cfd96856fecae5c84
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-04-05 15:11:23 -0300

    Update script.js

---
+++ funçoes-parametros-default/script.js
@@ -0,0 +1,5 @@
+function contarUmaHistoria(personagem = 'Akamaru', atividade = 'brincar com a bola', nome_dono = 'Shikamaru'){
+    document.write(`Era uma vez um cachorro chamado ${personagem}, ele gostava de ${atividade}, seu dono era o ${nome_dono} e eles viveram felizes para sempre`)
+}
+
+contarUmaHistoria('Patrick', 'morder as coisas',undefined)
