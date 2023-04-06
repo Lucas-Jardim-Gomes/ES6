819111e124c7daffe95593b2a719173443cb9405
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-04-06 09:09:48 -0300

    Update script.js

--- funções-arrow-function/script.js
+++ funções-arrow-function/script.js
@@ -1,5 +1,51 @@
+/*
 let quadrado = function(x = 5){
     return x * x
 }
+*/
 
-document.write( quadrado())
+
+/*
+let quadrado = x => {
+    return x * x
+}
+
+document.write( quadrado(3) + '<br/>')
+
+
+let circulo = (y = 4,z = 5) => y * z  //return implicito
+document.write(circulo())
+
+*/
+
+/*
+let parOuImpar = function(numero){
+    if(numero % 2 === 0){
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+document.write(parOuImpar(7))
+*/
+
+
+/*
+let parOuImpar = numero => {
+    if(numero % 2 === 0){
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+document.write(parOuImpar(8))
+
+*/
+
+
+let parOuImpar = numero => numero % 2 === 0 ? 'par' : 'impar' //return implicito
+
+document.write(parOuImpar(3))
+
