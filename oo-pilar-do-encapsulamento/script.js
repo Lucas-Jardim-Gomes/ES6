fcd224edb98824826d2dbd484f1159ec56e027ab
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-04-23 10:43:07 -0300

    Update script.js

--- oo-pilar-do-encapsulamento/script.js
+++ oo-pilar-do-encapsulamento/script.js
@@ -1,6 +1,6 @@
 class Tv {
     constructor() {
-        this._relacaoCanais = Array( 2, 4, 5, 7, 10)
+        this._relacaoCanais = Array(2, 4, 5, 7, 10)
         this._canalAtivo = 5
         this._volume = 5
     }
@@ -13,7 +13,7 @@ class Tv {
     set canalAtivo(canal) {
         //
         if (this._relacaoCanais.indexOf(canal) !== -1) {
-        this._canalAtivo = canal
+            this._canalAtivo = canal
         }
     }
 }
