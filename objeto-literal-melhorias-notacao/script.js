da04c2b63ba856ec28e0f3fa00acc157504e1e8a
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-06-23 12:44:34 -0300

    Update script.js

---
+++ objeto-literal-melhorias-notacao/script.js
@@ -0,0 +1,32 @@
+let nome = 'Lucas' 
+let idade = 23
+let sexo = 'Masculino'
+let profissao = 'Programador'
+
+let objeto = {
+    nome: nome,
+    idade: idade,
+    sexo: sexo,
+    profissao: profissao,
+    exibirResumo: function() {
+        console.log(`${this.nome}, ${this.idade} anos, ${this.sexo} é ${this.profissao}`)
+    }
+}
+
+console.log(objeto)
+objeto.exibirResumo()
+
+
+let objeto2 = {
+    nome,
+    idade,
+    sexo,
+    profissao,
+    exibirResumo: function() {
+        console.log(`${this.nome}, ${this.idade} anos, ${this.sexo} é ${this.profissao}`)
+    }
+}
+
+
+console.log(objeto2)
+objeto.exibirResumo()
