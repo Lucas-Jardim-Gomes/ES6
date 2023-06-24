4783c70f04c8f0a629d00b393527438328617bbe
Lucasdev07 <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2023-06-24 09:46:06 -0300

    Create script.js

---
+++ objeto-literal-inclusão-atributos/script.js
@@ -0,0 +1,15 @@
+let pessoa = {
+    nome: 'Afonso',
+    idade: 35
+}
+console.log(pessoa)
+
+//-------------------------------
+
+pessoa.sexo = 'Masculino'
+
+console.log(pessoa)
+
+pessoa.dizerOi = () => console.log('Oi, tudo bem?')
+
+pessoa.dizerOi()
