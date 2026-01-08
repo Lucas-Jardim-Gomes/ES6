856fa472ad6786fbd028453339c8c9afe48d83bc
Lucas-Jardim-Gomes <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2026-01-08 15:20:40 -0300

    cria função construtora

---
+++ funcao-construtora/script.js
@@ -0,0 +1,26 @@
+    let Carro = function() {
+    this.cor = 'Gray';
+    this.modelo = 'BYD';
+    this.velocidadeAtual = 0;
+    this.velocidadeMaxima = 200;
+
+        this.acelerar = function() {
+           // this.velocidadeAtual += 10
+
+           let velocidade =this.getVelocidadeAtual() + 10
+              this.setVelocidadeAtual(velocidade)
+        }
+        this.getVelocidadeAtual = function() {
+            return this.velocidadeAtual
+    }
+
+    this.setVelocidadeAtual = function(velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual 
+    }
+
+}
+    let carro = new Carro()
+
+    console.log('A velocidade atual é:' + carro.getVelocidadeAtual() )
+
+
